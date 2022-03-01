1d2d71c895181e68a6276cba810b8fd5aed02b9e
lukebeddow <lukebeddow@gmail.com>
2022-03-01 09:38:54 +0000

    apply gitignore

--- build/depends/gripper.d
+++
@@ -1,1 +0,0 @@
-build/gripper.o build/cpp/gripper.o build/py/gripper.o build/depends/gripper.d : src/gripper.cpp src/gripper.h
--- build/depends/myfunctions.d
+++
@@ -1,9 +0,0 @@
-build/myfunctions.o build/cpp/myfunctions.o build/py/myfunctions.o build/depends/myfunctions.d : src/myfunctions.cpp src/myfunctions.h \
- /home/luke/.mujoco/mujoco210/include/mjxmacro.h \
- /home/luke/.mujoco/mujoco210/include/mujoco.h \
- /home/luke/.mujoco/mujoco210/include/mjmodel.h \
- /home/luke/.mujoco/mujoco210/include/mjdata.h \
- /home/luke/.mujoco/mujoco210/include/mjvisualize.h \
- /home/luke/.mujoco/mujoco210/include/mjrender.h \
- /home/luke/.mujoco/mujoco210/include/mjui.h src/customtypes.h \
- src/slidingwindow.h src/gripper.h src/mynum.h src/objecthandler.h
