dfa89e6e8d28c2b767c35fb1b8fce9108eaea242
Luke Beddow <lukebeddow@gmail.com>
2023-10-04 17:40:22 +0100

    brief extra layers training

--- rl/launch_training.py
+++ rl/launch_training.py
@@ -789,6 +789,49 @@ if __name__ == "__main__":
     tm.run_training(agent, env)
     print_time_taken()
 
+  elif args.program == "ppo_force_limit_2":
+
+    # define what to vary this training, dependent on job number
+    vary_1 = [2.0, 3.0, 3.458, 5.0]
+    vary_2 = [10.0, 30.0]
+    vary_3 = None
+    repeats = 4
+    tm.param_1_name = "bending frc limit"
+    tm.param_2_name = "palm frc limit"
+    tm.param_3_name = None
+    tm.param_1, tm.param_2, tm.param_3 = vary_all_inputs(args.job, param_1=vary_1, param_2=vary_2,
+                                                         param_3=vary_3, repeats=repeats)
+    if args.print: print_training_info()
+
+    # apply environment dependent settings
+    tm.settings["cpp"]["continous_actions"] = True
+    tm.settings["cpp"]["action"]["gripper_prismatic_X"]["value"] = 2e-3
+    tm.settings["cpp"]["action"]["gripper_revolute_Y"]["value"] = 0.015
+    tm.settings["cpp"]["action"]["gripper_Z"]["value"] = 4e-3
+    tm.settings["cpp"]["action"]["base_Z"]["value"] = 2e-3
+
+    # apply bending force limit
+    tm.settings["cpp"]["stable_finger_force_lim"] = tm.param_1
+    tm.settings["cpp"]["stable_palm_force_lim"] = tm.param_2
+
+    # create the environment
+    env = tm.make_env()
+    
+    # apply the agent settings
+    layers = [128, 128, 128]
+    tm.settings["Agent_PPO"]["learning_rate_pi"] = 1e-5
+    tm.settings["Agent_PPO"]["learning_rate_vf"] = 1e-5
+    network = MLPActorCriticPG(env.n_obs, env.n_actions, hidden_sizes=layers,
+                                continous_actions=True)
+
+    # make the agent
+    agent = Agent_PPO(device=args.device)
+    agent.init(network)
+
+    # complete the training
+    tm.run_training(agent, env)
+    print_time_taken()
+
   elif args.program == "example_template":
 
     # define what to vary this training, dependent on job number
