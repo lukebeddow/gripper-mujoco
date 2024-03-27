try to fix MAT logprob

rl/Trainer.py
@@ -13,7 +13,7 @@ import functools
 from ModelSaver import ModelSaver
 from agents.DQN import Agent_DQN
 from agents.ActorCritic import MLPActorCriticAC, Agent_SAC
-from agents.PolicyGradient import MLPActorCriticPG, Agent_PPO
+from agents.PolicyGradient import MLPActorCriticPG, Agent_PPO, Agent_PPO_MAT
 from env.MjEnv import MjEnv
 import networks
 
@@ -33,7 +33,7 @@ class TrackTraining:
     self.plot_episode_time = False
     self.plot_train_raw = False
     self.plot_train_avg = True
-    self.plot_test_raw = False
+    self.plot_test_raw = True
     self.plot_test_metrics = False
     # general
     self.episodes_done = 0
@@ -61,7 +61,7 @@ class TrackTraining:
     self.fig = None
     self.axs = None
 
-  def add_test_metrics(self, metrics_to_add, dtype=None):
+  def add_test_metrics(self, metrics_to_add, dtype=None, values=None):
     """
     Include additional test metrics
     """
@@ -72,10 +72,16 @@ class TrackTraining:
 
     if isinstance(metrics_to_add, str):
       metrics_to_add = [metrics_to_add]
+    if isinstance(values, np.ndarray):
+      values = [values]
 
-    for m in metrics_to_add:
+    for i, m in enumerate(metrics_to_add):
       self.test_metric_names.append(m)
-      self.test_metric_values.append(np.array([], dtype=dtype))
+      if values is None:
+        thisvalue = np.array([], dtype=dtype)
+      else:
+        thisvalue = values[i]
+      self.test_metric_values.append(thisvalue)
 
     self.n_test_metrics = len(self.test_metric_names)
 
@@ -195,7 +201,7 @@ class TrackTraining:
           self.axs.append([axs4, axs4]) # add paired to hold the pattern
         if self.plot_test_metrics:
           for i in range(self.n_test_metrics):
-            fig5, axs5 = plt.subplots(2, 1)
+            fig5, axs5 = plt.subplots(1, 1)
             self.fig.append(fig5)
             self.axs.append([axs5, axs5]) # add paired to hold the pattern
 
@@ -580,10 +586,10 @@ class Trainer:
       else: self.curriculum_dict["stage"] = 0
 
       # load in the curriculum functions
-      if "curriculum_change_fcn" in load_train["curriculum_dict"].keys():
-        self.curriculum_change_fcn = functools.partial(load_train["curriculum_dict"]["curriculum_change_fcn"], self)
-      if "curriculum_fcn" in load_train["curriculum_dict"].keys():
-        self.curriculum_fcn = functools.partial(load_train["curriculum_dict"]["curriculum_fcn"], self)
+      if "curriculum_change_fcn" in load_train.keys():
+        self.curriculum_change_fcn = load_train["curriculum_change_fcn"]
+      if "curriculum_fcn" in load_train.keys():
+        self.curriculum_fcn = load_train["curriculum_fcn"]
       else:
         # TEMPORARY FIX for program: mat_liftonly
         if self.group_name == "22-03-24":

rl/agents/PolicyGradient.py
@@ -14,6 +14,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions.normal import Normal
 from torch.distributions.categorical import Categorical
+from torch.distributions.bernoulli import Bernoulli
 from torchaudio.functional import lfilter
 
 # ----- helper functions, networks, data buffers ----- #
@@ -405,7 +406,7 @@ class PPOBuffer:
                 adv=self.advantages, logp=self.logprobs)
     return data
 
-# ----- actor critic network building blocks ----- #
+# ----- base actor critic network building blocks ----- #
 
 class Actor(nn.Module):
 
@@ -502,7 +503,7 @@ class MLPActorCriticPG(nn.Module):
     # policy builder depends on action space
     if continous_actions:
       self.action_dim = action_dim
-      self.pi= MLPGaussianActor(n_obs, action_dim, hidden_sizes, activation, device=device)
+      self.pi = MLPGaussianActor(n_obs, action_dim, hidden_sizes, activation, device=device)
     else:
       self.action_dim = 1 # discrete so only one action
       self.pi = MLPCategoricalActor(n_obs, action_dim, hidden_sizes, activation, device=device)
@@ -549,6 +550,8 @@ class MLPActorCriticPG(nn.Module):
     self.mode = "test"
     self.pi.eval()
     self.vf.eval()
+
+# ----- CNN and multi-input actor critics ----- #
 
 class CNNCategoricalActor(Actor):
     
@@ -794,6 +797,131 @@ class NetActorCriticPG(nn.Module):
     with torch.no_grad():
       pi = self.pi._distribution(obs)
       a = pi.sample()
+      logp_a = self.pi._log_prob_from_distribution(pi, a)
+      val = self.vf(obs)
+    return a, val, logp_a
+
+  def act(self, obs):
+    return self.step(obs)[0]
+
+  def set_device(self, device):
+    self.pi.to_device(device)
+    self.vf.to_device(device)
+    self.device = device
+
+  def training_mode(self):
+    """
+    Put the agent into training mode
+    """
+    self.mode = "train"
+    self.pi.train()
+    self.vf.train()
+
+  def testing_mode(self):
+    """
+    Put the agent in testing mode
+    """
+    self.mode = "test"
+    self.pi.eval()
+    self.vf.eval()
+
+# ----- MAT specific ----- #
+    
+class MATActor(Actor):
+
+  def __init__(self, obs_dim, act_dim, hidden_sizes, activation, device="cpu",
+               use_extra_actions=True):
+    super().__init__()
+    log_std = -0.5 * np.ones(1, dtype=np.float32) # only for wrist action
+    self.log_std = torch.nn.Parameter(torch.as_tensor(log_std))
+    self.net = mlp([obs_dim] + list(hidden_sizes) + [act_dim], activation)
+    self.sigmoid = nn.Sigmoid()
+    self.use_extra_actions = use_extra_actions
+    self.to_device(device)
+
+  def _distribution(self, obs):
+    act = self.net(obs) # run observation through network
+    if self.use_extra_actions:
+      sigact = self.sigmoid(act[:, :-1]) # exclude wrist
+      wrist_mu = act[:,-1] # exctract wrist action mean
+      # log_std stays on cpu so we need to move this to our device
+      std = torch.exp(self.log_std).to(self.device) # convert from log->regular
+      return (Bernoulli(probs=sigact), Normal(wrist_mu, std))
+    else:
+      sigact = self.sigmoid(act)
+      return Bernoulli(probs=sigact)
+
+  def _log_prob_from_distribution(self, pi, act):
+    """Calculate the log prob using the equation from the MAT paper"""
+    if self.use_extra_actions:
+      finger_probs = pi[0].probs[:,:-2] # exclude lift and reopen (pi[0] has no wrist)
+      lift_probs = pi[0].probs[:,-2]
+      reopen_probs = pi[0].probs[:,-1]
+      lift_act = act[:,-3]
+      reopen_act = act[:,-2]
+      wrist_act = act[:,-1] # excluded from logprob calculation
+      logprob = (
+        torch.log(reopen_probs) 
+        + (1-reopen_act) * (torch.log(lift_probs))
+        + (1-reopen_act) * (1-lift_act) * (torch.sum(torch.log(finger_probs), axis=1))
+      )
+    else:
+      finger_probs = pi.probs[:,:-1] # exclude lift
+      lift_probs = pi.probs[:,-1]
+      lift_act = act[:,-1]
+      logprob = (
+        torch.log(lift_probs)
+        + (1-lift_act) * (torch.sum(torch.log(finger_probs), axis=1))
+      )
+    return logprob
+  
+  def to_device(self, device=None):
+    if device is None:
+      device = self.device
+    self.net.to(device)
+    self.device = device
+
+class MATActorCriticPG(nn.Module):
+
+  name = "MLPActorCriticPG_"
+
+  def __init__(self, n_obs, action_dim, hidden_sizes=(128, 128, 128, 128, 128, 128),
+                activation=nn.ReLU, mode="train", device="cpu", use_extra_actions=True):
+    super().__init__()
+
+    self.n_obs = n_obs
+    self.n_actions = action_dim
+    self.mode = mode
+    self.device = device
+
+    self.action_dim = action_dim
+    self.pi = MATActor(n_obs, action_dim, hidden_sizes, activation, device=device,
+                            use_extra_actions=use_extra_actions)
+
+    # build value function - use paper specified hidden sizes
+    self.vf  = MLPCritic(n_obs, (128, 128, 128), activation)
+
+    if self.mode not in ["test", "train"]:
+      raise RuntimeError(f"MLPActorCriticPG given mode={mode}, should be 'test' or 'train'")
+
+    # add hidden layer size into the name
+    for i in range(len(hidden_sizes)):
+      if i == 0: self.name += f"{hidden_sizes[i]}"
+      if i > 0: self.name += f"x{hidden_sizes[i]}"
+
+  def step(self, obs):
+    with torch.no_grad():
+      pi = self.pi._distribution(obs)
+      if self.pi.use_extra_actions:
+        a_1 = pi[0].sample()
+        a_2 = pi[1].sample()
+        # if reopen action is triggered, lift must be zero
+        reopen_triggered = a_1[:,-1] > 0.5
+        a_1[reopen_triggered, -2] = 0.0
+        # concatenate resultant action tensor
+        a = torch.concatenate((a_1, a_2.unsqueeze(0)), dim=1)
+      else:
+        a = pi.sample()
       logp_a = self.pi._log_prob_from_distribution(pi, a)
       val = self.vf(obs)
     return a, val, logp_a
@@ -1234,7 +1362,6 @@ class Agent_PPO_MAT:
 
     # options
     use_extra_actions: bool = True
-    use_MAT_logprob: bool = True
 
     # options not in use for exact paper implementation
     use_random_action_noise: bool = False # paper implied
@@ -1387,8 +1514,6 @@ class Agent_PPO_MAT:
     # store values from before the environment steps to put into buffer
     self.last_state = state
     self.last_value = value
-    if self.params.use_MAT_logprob:
-      logprob = self.compute_actlogp(action) # compute the MAT action logprob
     self.last_logprob = logprob 
 
     return action.squeeze(0) # from tensor([[x]]) to tensor([x])
@@ -1450,6 +1575,8 @@ class Agent_PPO_MAT:
     [B x A] where B is the batch number and A are the individual action vectors
     """
 
+    sigmoid = torch.nn.Sigmoid()
+
     # if we are using the extra actions [a_reopen, a_wrist, a_wrist_stdev]
     if self.params.use_extra_actions:
 
@@ -1457,7 +1584,8 @@ class Agent_PPO_MAT:
       action = action[:,:-2]
 
       # apply the sigmoid to all elements
-      actprobs = 1.0 / (1.0 + torch.exp(-action))
+      # actprobs = 1.0 / (1.0 + torch.exp(-action))
+      actprobs = sigmoid(action)
 
       # calculate the logprob
       actlogprob = (
@@ -1472,6 +1600,8 @@ class Agent_PPO_MAT:
 
       # apply the sigmoid to all elements
       actprobs = 1.0 / (1.0 + torch.exp(-action))
+
+      # avoid sigmoid for numerical instability
 
       # calculate the logprob
       actlogprob = (
@@ -1495,21 +1625,16 @@ class Agent_PPO_MAT:
     """
     obs, act, adv, logp_old = data['obs'], data['act'], data['adv'], data['logp']
 
-    # print("act is", act)
-    # print("act shape is", act.shape)
-
     # Policy loss
     pi, logp = self.mlp_ac.pi(obs, act)
-    if self.params.use_MAT_logprob:
-      logp = self.compute_actlogp(act) # compute the MAT action logprob
-      logp.requires_grad_(True) # enable gradient calculations
     ratio = torch.exp(logp - logp_old)
     soft_advantage = adv - self.params.temperature_alpha * logp # MAT addition, to make soft
     clip_ratio = torch.clamp(ratio, 1-self.params.clip_ratio, 1+self.params.clip_ratio)
     min_ratio = torch.min(ratio, clip_ratio)
     loss_pi = -(min_ratio * soft_advantage).mean()
 
-    # Useful extra info
+    # Useful extra info - should not be used with MAT!
+    if self.mlp_ac.pi.use_extra_actions: pi = pi[0]
     approx_kl = (logp_old - logp).mean().item()
     ent = pi.entropy().mean().item()
     clipped = ratio.gt(1+self.params.clip_ratio) | ratio.lt(1-self.params.clip_ratio)
@@ -1577,7 +1702,7 @@ class Agent_PPO_MAT:
       action,
       reward,
       self.last_value,
-      self.last_logprob * truncated # no prob if max_step_num exceeded
+      self.last_logprob * (not truncated) # no prob if max_step_num exceeded
     )
 
     self.steps_done += 1

rl/launch_training.py
@@ -16,8 +16,9 @@ from Trainer import MujocoTrainer
 from TrainingManager import TrainingManager
 from agents.DQN import Agent_DQN
 from agents.ActorCritic import MLPActorCriticAC, Agent_SAC
-from agents.PolicyGradient import MLPActorCriticPG, Agent_PPO, Agent_PPO_MAT
+from agents.PolicyGradient import MLPActorCriticPG, Agent_PPO
 from agents.PolicyGradient import CNNActorCriticPG, NetActorCriticPG, MixedNetworkFromEncoder
+from agents.PolicyGradient import Agent_PPO_MAT, MATActorCriticPG
 import networks
 
 def vary_all_inputs(raw_inputarg=None, param_1=None, param_2=None, param_3=None, repeats=None):
@@ -683,6 +684,8 @@ if __name__ == "__main__":
 
   args = parser.parse_args()
 
+  # exit()
+
   timestamp = args.timestamp if args.timestamp else datetime.now().strftime(datestr)
 
   # default device
@@ -709,7 +712,7 @@ if __name__ == "__main__":
 
   # seperate process for safety when running a training program
   if (not args.no_delay):
-    if (args.job is not None and
+    if (args.job is not None and not args.plot and
         (args.program is not None or args.resume is not None or args.test)):
       sleep_for = args.job - args.smallest_job_num
       if sleep_for < 0: sleep_for = args.job # in case of jobstr "4 5 6 1"
@@ -755,6 +758,11 @@ if __name__ == "__main__":
     if args.log_level > 0: print("launch_training.py will plot a training")
 
     tm.load(job_num=args.job, timestamp=args.timestamp, id=args.load_id)
+    tm.trainer.track.plot_train_avg = True
+    tm.trainer.track.plot_test_raw = True
+    tm.trainer.track.add_test_metrics(metrics_to_add=["Success rate"], 
+                                      values=[tm.trainer.track.avg_successful_grasp])
+    tm.trainer.track.plot_test_metrics = True
     tm.trainer.track.plot(plttitle=tm.group_name + "/" + tm.run_name)
     input("Press enter to quit plotting windows and terminate program")
     exit()
@@ -4590,19 +4598,22 @@ if __name__ == "__main__":
       (1e-6, 5e-5),
       (1e-5, 5e-5),
       (1e-4, 5e-5),
+      (1e-3, 5e-5),
       (1e-6, 5e-4),
       (1e-5, 5e-4),
       (1e-4, 5e-4),
+      (1e-3, 5e-4),
       (1e-6, 5e-3),
       (1e-5, 5e-3),
       (1e-4, 5e-3),
+      (1e-3, 5e-3),
     ]
     vary_2 = [150, 300, 600]
-    vary_3 = [(10, False), (80, True)]
-    repeats = 1
+    vary_3 = [False, True]
+    repeats = 3
     tm.param_1_name = "lr/temperature"
     tm.param_2_name = "steps per epoch"
-    tm.param_3_name = "iters/use_kl"
+    tm.param_3_name = "extra actions"
     tm.param_1, tm.param_2, tm.param_3 = vary_all_inputs(args.job, param_1=vary_1, param_2=vary_2,
                                                          param_3=vary_3, repeats=repeats)
     if args.print: print_training_info()
@@ -4664,21 +4675,124 @@ if __name__ == "__main__":
     tm.settings["Agent_PPO_MAT"]["learning_rate_pi"] = tm.param_1[0]  # 1e-4 paper specified
     tm.settings["Agent_PPO_MAT"]["learning_rate_vf"] = tm.param_1[0]  # 1e-4 paper specified
     tm.settings["Agent_PPO_MAT"]["steps_per_epoch"] = tm.param_2
-    tm.settings["Agent_PPO_MAT"]["train_pi_iters"] = tm.param_3[0]
-    tm.settings["Agent_PPO_MAT"]["train_vf_iters"] = tm.param_3[0]
-    tm.settings["Agent_PPO_MAT"]["use_KL_early_stop"] = tm.param_3[1]
-
-    # NEW: TEST WITHOUT NEW LOGPROB CALCULATION
-    tm.settings["Agent_PPO_MAT"]["use_MAT_logprob"] = False
 
     # create the environment
     env = tm.make_env()
 
     # apply the agent settings
-    layers = [128 for i in range(6)]
-    n = 3 if tm.settings["env"]["MAT_use_reopen"] else 0
-    network = MLPActorCriticPG(env.n_obs, env.n_actions + n, hidden_sizes=layers,
-                                continous_actions=True)
+    layers = [128 for i in range(6)] # only applies to pi, vf is set to [3*128]
+    n = 2 if use_extra_actions else 0
+    network = MATActorCriticPG(env.n_obs, env.n_actions + n, hidden_sizes=layers,
+                                use_extra_actions=use_extra_actions)
+    
+    # make the agent
+    agent = Agent_PPO_MAT(device=args.device)
+    agent.init(network)
+
+    # complete the training
+    tm.run_training(agent, env)
+
+    # add an extra test on the old object set
+    tm.run_test(trials_per_obj=20, different_object_set="set8_fullset_1500",
+                load_best_id=True)
+    
+    print_time_taken()
+
+  elif args.program == "panic_check":
+
+    # define what to vary this training, dependent on job number
+    vary_1 = [
+      (1e-6, 5e-5),
+      (1e-5, 5e-5),
+      (1e-4, 5e-5),
+      (1e-3, 5e-5),
+      (1e-6, 5e-4),
+      (1e-5, 5e-4),
+      (1e-4, 5e-4),
+      (1e-3, 5e-4),
+      (1e-6, 5e-3),
+      (1e-5, 5e-3),
+      (1e-4, 5e-3),
+      (1e-3, 5e-3),
+    ]
+    vary_2 = [False, True]
+    vary_3 = None
+    repeats = 1
+    tm.param_1_name = "lr/temperature"
+    tm.param_2_name = "extra actions"
+    tm.param_3_name = None
+    tm.param_1, tm.param_2, tm.param_3 = vary_all_inputs(args.job, param_1=vary_1, param_2=vary_2,
+                                                         param_3=vary_3, repeats=repeats)
+    if args.print: print_training_info()
+
+    # apply env settings
+    use_extra_actions = tm.param_2
+    tm.settings["trainer"]["num_episodes"] = 120_000
+    tm.settings["env"]["use_MAT"] = True
+    tm.settings["env"]["MAT_use_reopen"] = use_extra_actions
+    tm.settings["env"]["max_episode_steps"] = 250 # Horizon = 250 in paper, see eq.5
+    # tm.settings["env"]["base_lim_yaw_rad"] = np.pi / 4 # reduce from [-pi, +pi] in paper due to symettry
+    tm.settings["env"]["finger_thickness"] = 1.0e-3 # put same fingers on as TMech eval
+    tm.settings["env"]["finger_width"] = 24e-3
+    tm.settings["env"]["finger_hook_angle_degrees"] = 75
+    tm.settings["env"]["XY_base_actions"] = True # enable for xml, but not actions
+    tm.settings["env"]["Z_base_rotation"] = True # enable for xml, but not actions
+
+    # apply state and sensor settings
+    tm.settings["cpp"]["sensor_n_prev_steps"] = 20
+    tm.settings["cpp"]["state_n_prev_steps"] = 20
+    tm.settings["cpp"]["sensor_sample_mode"] = 6 # scaled square of change, saturates at 0.1 change
+    tm.settings["cpp"]["state_sample_mode"] = 4 # binary change information, no threshold, 0.05 in paper
+    tm.settings["cpp"]["sensor"]["cartesian_contacts_XYZ"]["in_use"] = True
+    tm.settings["cpp"]["sensor"]["cartesian_contacts_XYZ"]["noise_override"] = [0, 0]
+    
+    # # possible to change noise settings to improve learning
+    # tm.settings["cpp"]["sensor_noise_std"] = 0.01 # reduce based on real life
+
+    # apply action settings
+    tm.settings["cpp"]["use_termination_action"] = True # for final lift
+
+    # turn on or off Z height
+    use_Z = False
+    tm.settings["cpp"]["sensor"]["base_state_sensor_Z"]["in_use"] = use_Z
+    tm.settings["cpp"]["action"]["base_Z"]["in_use"] = use_Z
+    tm.settings["cpp"]["sensor"]["wrist_sensor_Z"]["in_use"] = use_Z
+    if not use_Z:
+      tm.settings["env"]["fingertip_clearance"] = 5e-3 # closer to ground given no Z height changes. Original=10e-3
+      tm.settings["cpp"]["base_position_noise"] = 0e-3 # disable base position noise? Original=5e-3
+
+    # apply reward settings
+    tm.settings["reward"]["style"] = "MAT_liftonly"
+    tm.settings["reward"]["penalty_termination"] = True # do we end early for oob and dangerous forces
+    tm.settings["reward"]["stable_trigger"] = 1
+
+    # enable the curriculum of step size adjustments
+    tm.settings["trainer"]["use_curriculum"] = True
+    tm.settings["curriculum"]["whole_fcn_override"] = curriculum_fcn_MAT
+    tm.settings["curriculum"]["param_values"] = [
+      [2e-3, 8e-3], # gripper_X action min/max
+      [0.015, 0.06], # gripper_Y action min/max
+      [4e-3, 16e-3], # gripper_Z action min/max
+      [2e-3, 8e-3], # base_Z action min/max
+      [0.2, 0.8],   # time per action min/max
+    ]
+
+    # apply agent hyperparameters
+    tm.settings["Agent_PPO_MAT"]["use_extra_actions"] = use_extra_actions
+    tm.settings["Agent_PPO_MAT"]["learning_rate_pi"] = tm.param_1[0]  # 1e-4 paper specified
+    tm.settings["Agent_PPO_MAT"]["learning_rate_vf"] = tm.param_1[0]  # 1e-4 paper specified
+    tm.settings["Agent_PPO_MAT"]["steps_per_epoch"] = 300
+
+    # create the environment
+    env = tm.make_env()
+
+    tm.settings["trainer"]["test_freq"] = 1000
+
+    # apply the agent settings
+    layers = [128 for i in range(6)] # only applies to pi, vf is set to [3*128]
+    n = 2 if use_extra_actions else 0
+    network = MATActorCriticPG(env.n_obs, env.n_actions + n, hidden_sizes=layers,
+                                use_extra_actions=use_extra_actions)
     
     # make the agent
     agent = Agent_PPO_MAT(device=args.device)